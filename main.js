845cc0f357aad7ad0a3fcc43affeee264d43bd98
James Little <littleguy23@gmail.com>
2016-01-30 11:29:02 -0500

    comments are cool

--- main.js
+++ main.js
@@ -1,32 +1,53 @@
 $(document).ready(function() {
+    // Find all links in h2 tags
     $("h2 > a").each(function(i) {
+
+        // Set it to a variable
         var titleLink = this;
+
+        // Find the title
         var title = $(this).html().trim();
+
+        // And then classify the title
         var classification = classify(title);
         if(classification === "listicle") {
             var url = $(this).attr("href");
             $.get(url, function(data) {
-                james(title, data, titleLink);
+                analyzeListicle(title, data, titleLink);
             });
         }
     });
 })
 
 
-function james(title, data, domElement) {
+function analyzeListicle(title, data, domElement) {
+    // JQuery can turn strings into a DOM, so we do that
+    // with the article's HTML
     var nextDom = $.parseHTML($.trim(data));
-
     var newDom = jQuery(nextDom);
+
+    // overlayText is a variable that contains the HTML
+    // content for the entire hover box that appears
     var overlayText = '';
+
+    // For each list item text in the listicle
     $('.subbuzz_name', newDom).each( function() {
+
+        // listItem is the inner HTML of the list item
         var listItem = $(this).html().trim();
+
+        // Add a wrapper div and the list item text to overlayText
         overlayText += '<div class="hack-list-item">' + listItem;
 
         // Check if it's an image post
         var lastChar = listItem[listItem.length-1];
         if (lastChar === ':') {
+            // Add a space and the little image icon
             overlayText += "&nbsp;" + icon();
-            var content = '<img class="hack-embedded-img" src="http://placehold.it/200x200">';
+
+            // Find the image associated with the list item
+            // and add it to overlayText
+            var content = '<img class="hack-embedded-img" src="http://placehold.it/200x200&text=">';
             overlayText += content;
         }
 
@@ -35,6 +56,8 @@ function james(title, data, domElement) {
     overlay(domElement, overlayText);
 }
 
+// Determine what kind of article it is, based on headline
+// text analysis
 function classify(headline) {
     var list = /^(The )?\d+ .*$/;
     if(list.test(headline)) {

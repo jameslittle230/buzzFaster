97a2fa368be180e3e1a8e656722b0b3485116901
James Little <littleguy23@gmail.com>
2016-01-29 22:29:43 -0500

    Extract list items and attach them

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 $("h2 > a").each(function(i) {
+    var titleLink = this;
     var title = $(this).html().trim();
     var classification = classify(title);
     if(classification === "listicle") {
         var url = $(this).attr("href");
         $.get(url, function(data) {
-            james(title, data);
+            james(title, data, titleLink);
         });
         overlay(this, "Okay guise");
     }
 });
 
-function james(title, data) {
-    // console.group();
-    // console.log(title)
+function james(title, data, domElement) {
+    console.log(domElement);
+    console.group();
+    console.log(title)
     var nextDom = $.parseHTML($.trim(data));
 
     var newDom = jQuery(nextDom);
-    // console.log($('h2', newDom).html());
-    // console.groupEnd();
+    $('.subbuzz_name', newDom).each( function() {
+        var listItem = $(this).html().trim();
+        $(domElement).append('<br>' + listItem);
+        // console.log($(this).attr('class'));
+    });
+    console.groupEnd();
 }
 
 function classify(headline) {

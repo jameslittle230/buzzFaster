cb8fcd4493d1ccac423a6fa8f0d200655e77c4fc
James Little <littleguy23@gmail.com>
2016-01-29 21:57:09 -0500

    Debug classification function

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 $("h2 > a").each(function(i) {
-    var title = $(this).html();
-    var url = $(this).attr("href");
-    $.get(url, function(data) {
-        james(title, data);
-    });
+    var title = $(this).html().trim();
+    var classification = classify(title);
+    if(classification === "listicle") {
+        var url = $(this).attr("href");
+        $.get(url, function(data) {
+            james(title, data);
+        });
+    }
 });
 
 function james(title, data) {
-    console.group();
-    console.log(title)
+    // console.group();
+    // console.log(title)
     var nextDom = $.parseHTML($.trim(data));
 
     var newDom = jQuery(nextDom);
-    console.log($('h2', newDom).html());
-    console.groupEnd();
+    // console.log($('h2', newDom).html());
+    // console.groupEnd();
 }
 
 function classify(headline) {
-    var list = /^\d+ \w+s .*$/;
+    var list = /^\d+ .*$/;
     if(list.test(headline)) {
-        console.log(headline);
+        console.log(headline + " is a listicle");
         return "listicle";
+    } else {
+        console.error(headline + " is not a listicle");
     }
     return "other";
 }

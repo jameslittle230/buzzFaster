7a68f74036a67ad53ee0f04c5d8fb7c3cfa71cde
James Little <littleguy23@gmail.com>
2016-01-30 18:21:02 -0500

    Delete files

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, callback) {
-    console.log(request.url);
-    $.get(request.url).done(function(data) {
-        callback(data);
-    });
-    console.log("Welp");
-    return true;
-});
